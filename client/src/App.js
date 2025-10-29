dcacf5d57ab0887a68d2bc1fca674fe758ec013a
Karina Jhingan <karinajhingan@Karinas-MacBook-Pro.local>
2025-10-28 21:59:39 -0700

    Set up React frontend and Node.js backend

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React + Node.js App</h1>
+        <p>{message || "Loading message from server..."}</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
